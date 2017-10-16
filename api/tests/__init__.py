0c8ac66e01f7cd721cfdb00e148312157f07d338
Julia <juliesergienko@gmail.com>
2017-10-16 12:31:27 +0300

    environment set up

--- api/tests/__init__.py
+++ api/tests/__init__.py
@@ -3,4 +3,6 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
+from api.tests.urls import EnvironmentTestCase
+
 # Create your tests here.
---
+++ api/tests/urls.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+
+
+class EnvironmentTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    # def test_django_rest_in_installed_apps(self):
+    #     from .settings import INSTALLED_APPS
+    #     self.assertTrue('rest_framework' in INSTALLED_APPS)
+
+    def test_smthing(self):
+        pass
